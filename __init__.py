22bb5e988c2dfefc559e3ab168edef30eaddba62
Stav Sapir <stavsap@gmail.com>
2023-10-17 23:32:07 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,31 +1,4 @@
-import importlib.util
-import glob
-import os
-import sys
-import traceback
+NODE_CLASS_MAPPINGS = {"test"}
+NODE_DISPLAY_NAME_MAPPINGS = {"test"}
 
-
-NODE_CLASS_MAPPINGS = {}
-NODE_DISPLAY_NAME_MAPPINGS = {}
-
-def init():
-    py = os.path.dirname(__file__)
-    py = os.path.abspath(dir)
-    files = glob.glob("*.py", root_dir=py, recursive=False)
-
-    for file in files:
-        try:
-            name = os.path.splitext(file)[0]
-            spec = importlib.util.spec_from_file_location(name, os.path.join(py, file))
-            module = importlib.util.module_from_spec(spec)
-            sys.modules[name] = module
-            spec.loader.exec_module(module)
-            if hasattr(module, "NODE_CLASS_MAPPINGS") and getattr(module, "NODE_CLASS_MAPPINGS") is not None:
-                NODE_CLASS_MAPPINGS.update(module.NODE_CLASS_MAPPINGS)
-                if hasattr(module, "NODE_DISPLAY_NAME_MAPPINGS") and getattr(module, "NODE_DISPLAY_NAME_MAPPINGS") is not None:
-                    NODE_DISPLAY_NAME_MAPPINGS.update(module.NODE_DISPLAY_NAME_MAPPINGS)
-        except Exception as e:
-            traceback.print_exc()
-
-init()
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
